23cc3663c2f5a0028e5a59019292de02663ae931
HuyNguyen <thehuy260996@gmail.com>
2021-05-08 11:49:43 +0700

    update

--- GameProject/assets/script/jumpCharacter.js
+++ GameProject/assets/script/jumpCharacter.js
@@ -5,25 +5,26 @@ cc.Class({
     properties: {
         _jumping: false,
         _nextPosition: 0,
+        _currentPosition: 0,
+        force : 0,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
         this.node.getComponent(cc.Animation).play("RunAnim");
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP,this.jumpPlayer,this);
-        cc.director.getPhysicsManager().enabled = true;
-        this.node.getComponent(cc.RigidBody).fixedRotation  = true;
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN,this.jumpPlayer,this);
+        // cc.director.getPhysicsManager().enabled = true;
+        // this.node.getComponent(cc.RigidBody).fixedRotation  = true;
     },
 
     jumpPlayer(event){
         switch(event.keyCode){
             case cc.macro.KEY.space:
                 if(this._jumping === false){
-                    this.node.getComponent(cc.Animation).play("JumpAnim");
-                    this.node.setPosition(cc.v2(this.node.x, this.node.y + 200));
-                    this.node.getComponent(cc.RigidBody).applyForceToCenter(new cc.Vec2(0,100000),true);              
                     this._jumping = true;
+                    this.node.getComponent(cc.Animation).play("RunAnim");
+                    this.node.getComponent(cc.RigidBody).applyForceToCenter(new cc.Vec2(0, this.force += 250000), true);
                 }
             break;
         }
@@ -35,17 +36,21 @@ cc.Class({
 
     update (dt) {
         
-        if(this._jumping === true)
-        {
-            if(this.node.position.y > this._nextPosition)
-            {
-                this.node.getComponent(cc.RigidBody).gravityScale = 10;
-                
-                this.node.getComponent(cc.Animation).play("RunAnim");
-                this.node.angle = 0;
-
-                this._jumping = false;
-            }
+        if(this._jumping === true){
+            
         }
+
+        // if(this._jumping === true)
+        // {
+        //     if(this.node.position.y > this._nextPosition)
+        //     {
+        //         this.node.getComponent(cc.RigidBody).gravityScale = 10;
+                
+        //         this.node.getComponent(cc.Animation).play("RunAnim");
+        //         this.node.angle = 0;
+
+        //         this._jumping = false;
+        //     }
+        // }
     },
 });
